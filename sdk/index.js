e7d87b1da72da53940c9d9d39c41152c4f1cb50e
Paul Soh <paulsoh@MacBook-Pro-2.local>
2018-07-23 11:39:04 +0900

    Comment out debuggin console.logs

--- sdk/index.js
+++ sdk/index.js
@@ -36,17 +36,30 @@ class SDK {
       const contractJsonInterface = fs.readFileSync(pathToContract)
       const { abi, bytecode } = JSON.parse(contractJsonInterface)
       const ContractToDeploy = new this.web3.eth.Contract(abi)
-      return ContractToDeploy.deploy({
-        data: bytecode
-      }).send(
-        {
-          from: "0x318c9588ba25c3aa9342182fa7c84a7ed20f34e4",
-          gas: 1500000,
-          gasPrice: "20000000000"
-        },
-        function(err, transactionHash) {
-          console.log(transactionHash)
-        }
+      return (
+        ContractToDeploy.deploy({
+          data: bytecode
+        })
+          .send({
+            from: fromAddress,
+            gas,
+            gasPrice
+          })
+          .on("error", function(error) {
+            console.error(error)
+          })
+          .on("transactionHash", function(transactionHash) {
+            console.log(transactionHash)
+          })
+          // .on("receipt", function(receipt) {
+          //   console.log(receipt.contractAddress) // contains the new contract address
+          // })
+          // .on("confirmation", function(confirmationNumber, receipt) {
+          //   console.log(confirmationNumber)
+          // })
+          .then(function(newContractInstance) {
+            return Promise.resolve(newContractInstance)
+          })
       )
     } catch (e) {
       console.error("Error while creating contract: " + e)
