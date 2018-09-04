f16bfb047e5c06f1b85ebedf8644253a3dcd0563
Paul Soh <paulsoh@MacBook-Pro-2.local>
2018-09-04 12:27:23 +0900

    Up the gas amount for transactions to be blocked

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  gas: 167540,
+  gas: 267540,
   gasPrice: "10000000000",
   adminAddress: "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1",
   httpProvider: "http://localhost:8545"
